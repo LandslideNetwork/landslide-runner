4796a59ba6857a525a95606b7d1ad81fff231ad8
Vasyl Naumenko <vnau108@gmail.com>
2024-09-25 18:07:30 +0300

    contracts deploy

--- tools/white_whale/main.go
+++ tools/white_whale/main.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	// blockchainID is the ID of the blockchain, which is used in the local RPC address
-	blockchainID = "HLbGCVPnJ4kuFbk7Rbwmkuh1VFMzvysQHT8eXJkp7LM7RNG2F"
+	blockchainID = "2CLtTpjAwFXGF96KC2XNJ5p7vH6noz4Jntujv4YV61r5ZH9xLc"
 )
 
 func main() {
@@ -69,6 +69,116 @@ func main() {
 
 	deployer := internal.NewContractDeployer(acc1, chainService, client, log)
 
+	// whale_lair.wasm
+	whaleLairCodeID, whaleLairAddr, err := deployer.UploadAndInstantiate(
+		map[string]interface{}{
+			"unbonding_period": fmt.Sprintf("%d", int64(time.Hour*24*7/time.Nanosecond)), // default value is 14 days, in nanoseconds
+			"growth_rate":      fmt.Sprintf("%f", math.Pow(2, 1.0/365)-1),                // this is the value when you interpolate the growth rate to 2X with 365 days of bonding
+			"bonding_assets": []map[string]interface{}{
+				{"native_token": map[string]string{"denom": client.GetDenom()}},
+				{"native_token": map[string]string{"denom": "factory/" + acc1.Address + "/test"}},
+			},
+		},
+		"./artifacts/whale_lair.wasm",
+		5000000,
+	)
+	if err != nil {
+		log.Fatal("error deploying contract", zap.Error(err))
+		return
+	}
+	log.Info("Whale Lair contract deployed", zap.String("address", whaleLairAddr))
+	log.Info("Whale Lair contract deployed", zap.Uint64("codeID", whaleLairCodeID))
+
+	// fee_collector.wasm
+	feeCollCodeID, feeCollAddr, err := deployer.UploadAndInstantiate(
+		map[string]interface{}{},
+		"./artifacts/fee_collector.wasm",
+		5000000,
+	)
+	if err != nil {
+		log.Fatal("error deploying contract", zap.Error(err))
+		return
+	}
+	log.Info("Fee collector contract deployed", zap.String("address", feeCollAddr))
+	log.Info("Fee collector contract deployed", zap.Uint64("codeID", feeCollCodeID))
+
+	// start time for epoch manager
+	startTime := fmt.Sprintf("%d", time.Now().Add(time.Hour*24).UnixNano())
+
+	// fee_distributor.wasm
+	feeDistCodeID, feeDistAddr, err := deployer.UploadAndInstantiate(
+		map[string]interface{}{
+			"bonding_contract_addr": whaleLairAddr,
+			"fee_collector_addr":    feeCollAddr,
+			"grace_period":          "21", // default value is 21 epochs
+			"distribution_asset": map[string]interface{}{
+				"native_token": map[string]string{"denom": client.GetDenom()},
+			},
+			"epoch_config": map[string]interface{}{
+				"duration":      fmt.Sprintf("%d", int64(time.Hour*24*7/time.Nanosecond)), // default value is 7 days, in nanoseconds
+				"genesis_epoch": startTime,
+			},
+		},
+		"./artifacts/fee_distributor.wasm",
+		5000000,
+	)
+	if err != nil {
+		log.Fatal("error deploying contract", zap.Error(err))
+		return
+	}
+	log.Info("Fee distributor contract deployed", zap.String("address", feeDistAddr))
+	log.Info("Fee distributor contract deployed", zap.Uint64("codeID", feeDistCodeID))
+
+	// incentive.wasm
+	incentiveCodeID, err := deployer.Upload("./artifacts/incentive.wasm", 5000000)
+	if err != nil {
+		log.Fatal("error deploying contract", zap.Error(err))
+		return
+	}
+	log.Info("Incentive contract deployed", zap.Uint64("codeID", incentiveCodeID))
+
+	// incentive_factory.wasm
+	incentiveFactoryCodeID, incentiveFactoryAddr, err := deployer.UploadAndInstantiate(
+		map[string]interface{}{
+			"fee_collector_addr":   feeCollAddr,
+			"fee_distributor_addr": feeDistAddr,
+			"create_flow_fee": map[string]interface{}{
+				"amount": "1000000000",
+				"info": map[string]interface{}{
+					"native_token": map[string]string{"denom": client.GetDenom()},
+				},
+			},
+			"max_concurrent_flows":   5,
+			"incentive_code_id":      incentiveCodeID,
+			"max_flow_epoch_buffer":  14,
+			"min_unbonding_duration": 86400,
+			"max_unbonding_duration": 31556926,
+		},
+		"./artifacts/incentive_factory.wasm",
+		5000000,
+	)
+	if err != nil {
+		log.Fatal("error instantiating contract", zap.Error(err))
+		return
+	}
+	log.Info("Incentive factory contract deployed", zap.String("address", incentiveFactoryAddr))
+	log.Info("Incentive factory contract deployed", zap.Uint64("codeID", incentiveFactoryCodeID))
+
+	// frontend_helper.wasm
+	frontendHelperCodeID, frontendHelperAddr, err := deployer.UploadAndInstantiate(
+		map[string]interface{}{
+			"incentive_factory": incentiveFactoryAddr,
+		},
+		"./artifacts/frontend_helper.wasm",
+		5000000,
+	)
+	if err != nil {
+		log.Fatal("error deploying contract", zap.Error(err))
+		return
+	}
+	log.Info("Frontend Helper contract deployed", zap.String("address", frontendHelperAddr))
+	log.Info("Frontend Helper contract deployed", zap.Uint64("codeID", frontendHelperCodeID))
+
 	// terra swap_token.wasm
 	tokenCodeID, tokenAddr, err := deployer.UploadAndInstantiate(
 		map[string]interface{}{
@@ -96,19 +206,6 @@ func main() {
 	log.Info("Token contract deployed", zap.String("address", tokenAddr))
 	log.Info("Token contract deployed", zap.Uint64("codeID", tokenCodeID))
 
-	// fee_collector.wasm
-	feeCollCodeID, feeCollAddr, err := deployer.UploadAndInstantiate(
-		map[string]interface{}{},
-		"./artifacts/fee_collector.wasm",
-		5000000,
-	)
-	if err != nil {
-		log.Fatal("error deploying contract", zap.Error(err))
-		return
-	}
-	log.Info("Fee collector contract deployed", zap.String("address", feeCollAddr))
-	log.Info("Fee collector contract deployed", zap.Uint64("codeID", feeCollCodeID))
-
 	// vault.wasm
 	vaultCodeID, err := deployer.Upload("./artifacts/vault.wasm", 5000000)
 	if err != nil {
@@ -135,7 +232,6 @@ func main() {
 	log.Info("Vault factory contract deployed", zap.String("address", vaultFactoryAddr))
 	log.Info("Vault factory contract deployed", zap.Uint64("codeID", vaultFactoryCodeID))
 
-	// vault_router.wasm
 	vaultRouterCodeID, vaultRouterAddr, err := deployer.UploadAndInstantiate(
 		map[string]interface{}{
 			"owner":              acc1.Address,
@@ -206,25 +302,40 @@ func main() {
 	log.Info("Router contract deployed", zap.String("address", routerAddr))
 	log.Info("Router contract deployed", zap.Uint64("codeID", routerCodeID))
 
-	// whale_lair.wasm
-	whaleLairCodeID, whaleLairAddr, err := deployer.UploadAndInstantiate(
+	// epoch_manager.wasm
+	epochManagerCodeID, epochManagerAddr, err := deployer.UploadAndInstantiate(
 		map[string]interface{}{
-			"unbonding_period": fmt.Sprintf("%d", int64(time.Hour*24*7/time.Nanosecond)), // default value is 14 days, in nanoseconds
-			"growth_rate":      fmt.Sprintf("%f", math.Pow(2, 1.0/365)-1),                // this is the value when you interpolate the growth rate to 2X with 365 days of bonding
-			"bonding_assets": []map[string]interface{}{
-				{"native_token": map[string]string{"denom": client.GetDenom()}},
-				{"native_token": map[string]string{"denom": "factory/" + acc1.Address + "/test"}},
+			"start_epoch": map[string]interface{}{
+				"id":         1,
+				"start_time": startTime,
+			},
+			"epoch_config": map[string]interface{}{
+				"duration":      fmt.Sprintf("%d", int64(time.Hour*24*7/time.Nanosecond)), // default value is 7 days, in nanoseconds
+				"genesis_epoch": startTime,
 			},
 		},
-		"./artifacts/whale_lair.wasm",
+		"./artifacts/epoch_manager.wasm",
 		5000000,
 	)
 	if err != nil {
 		log.Fatal("error deploying contract", zap.Error(err))
 		return
 	}
-	log.Info("Whale Lair contract deployed", zap.String("address", whaleLairAddr))
-	log.Info("Whale Lair contract deployed", zap.Uint64("codeID", whaleLairCodeID))
+	log.Info("Epoch Manager contract deployed", zap.String("address", epochManagerAddr))
+	log.Info("Epoch Manager contract deployed", zap.Uint64("codeID", epochManagerCodeID))
+
+	// fee_distributor_mock.wasm
+	feeDistMockCodeID, feeDistMockAddr, err := deployer.UploadAndInstantiate(
+		map[string]interface{}{},
+		"./artifacts/fee_distributor_mock.wasm",
+		5000000,
+	)
+	if err != nil {
+		log.Fatal("error deploying contract", zap.Error(err))
+		return
+	}
+	log.Info("Fee distributor mock contract deployed", zap.String("address", feeDistMockAddr))
+	log.Info("Fee distributor mock contract deployed", zap.Uint64("codeID", feeDistMockCodeID))
 
 	log.Info("All contracts deployed successfully")
 }
